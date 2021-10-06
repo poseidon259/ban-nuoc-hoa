0f3c6440fca28ccd8a13cd162ab9876bb38ba3a5
Nguyen Duc Manh <60069793+manhhhh8321@users.noreply.github.com>
2021-10-06 15:21:32 +0700

    add images

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -388,7 +388,7 @@
                     <div class="row">
                         <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="product__item">
-                                <div class="product__item__pic set-bg" data-setbg="{{url('public/frontend')}}/img/product/product-1.jpg">
+                                <div class="product__item__pic set-bg" data-setbg="{{url('public/frontend')}}/img/product/product-13.jpg">
                                     <ul class="product__item__pic__hover">
                                         <li><a href="#"><i class="fa fa-heart"></i></a></li>
                                         <li><a href="#"><i class="fa fa-retweet"></i></a></li>
