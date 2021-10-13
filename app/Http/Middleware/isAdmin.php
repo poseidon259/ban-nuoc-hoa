eb6dad2088facb217490d2a407dbe8d2148eb536
Nguyen Duc Manh <60069793+manhhhh8321@users.noreply.github.com>
2021-10-13 14:32:24 +0700

    Auth

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Http\Middleware\Auth;
+use Illuminate\Support\Facades\Auth;
 class isAdmin
 {
     /**
