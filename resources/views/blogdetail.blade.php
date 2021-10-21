feae2dd1c69ba82e32d2f986232e24cf3c59919e
Nguyen Duc Manh <60069793+manhhhh8321@users.noreply.github.com>
2021-10-21 22:20:50 +0700

    css

--- resources/views/blogdetail.blade.php
+++ resources/views/blogdetail.blade.php
@@ -2,6 +2,12 @@
 <style>
     .blog-div {
         text-align: center;
+        width: 90%;
+        
+    }
+
+    .blog-div img {
+        width: 30%;
     }
 </style>
 @section('child-ui')
@@ -9,7 +15,7 @@
         <div class="blog-div">
             <h2>{{$item->title}}</h2>
             <img src="{{url('public/frontend')}}/img/blog/{{$item->img}}" alt="">
-            
+            <p>{{$item->description}}</p>
         </div>
     @endforeach
 @endsection
