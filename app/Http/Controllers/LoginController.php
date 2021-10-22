8da3b7d85424070f0f49811fd6ed55e1399c1ac1
Nguyen Duc Manh <60069793+manhhhh8321@users.noreply.github.com>
2021-10-22 19:14:35 +0700

    d

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    protected $table = 'account';
+    protected $table = 'users';
     public function index() {
         return redirect()->route('login');
     }
